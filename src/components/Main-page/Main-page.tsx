96e944f79503370080ffef5f3390f6efeae8e3bf
Vlad Teterev <vladtett2000@gmail.com>
2021-03-21 22:39:53 +0300

    feat: create main page

--- src/components/Main-page/Main-page.tsx
+++ src/components/Main-page/Main-page.tsx
@@ -1,8 +1,40 @@
-import './Main-page.css'
+import "./Main-page.css";
 function MainPage() {
   return (
     <div className="main-page">
-     <h1>Hello</h1>
+      <header>
+        <h1>RSLang</h1>
+      </header>
+      <main>
+        <div className="main-info">
+          <h2>English</h2>
+          <p>
+            Приложение для изучения иностранных слов с техникой интервального
+            повторения, отслеживания индивидуального прогресса и мини-игр.
+            Правила интервального повторения: — интервал обучения по умолчанию -
+            24 часа; — пользователь допустил ошибку или нажал кнопку повтора - 2
+            часа; — пользователь нажал кнопку 'хорошо' - 48 часов; —
+            пользователь нажал кнопку 'легко' - 120 часов.
+          </p>
+        </div>
+        <div className="auth">
+          <form className="auth-form">
+            <h3>Login</h3>
+            <label htmlFor="Email">Email</label>
+            <input className="Email" type="text"/>
+            <label htmlFor="Password">Password</label>
+            <input className="Password" type="text"/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </main>
+      <footer>
+        <ul>
+          <li><a href="https://rs.school/js/">RSSchool</a></li>
+          <li className="rs-logo"></li>
+          <li><a href="https://github.com/ue4prog">Github</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
